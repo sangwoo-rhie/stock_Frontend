ab3754552e748391cd8e448a2541e2c8bd64d54d
Sangwoo rhie <powercom92@naver.com>
2023-10-01 17:59:35 +0900

    fix. app.js 수정

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ const path = require('path');
 const app = express();
 const port = 3001;
 
-let corsOptions = {
-  origin: '*',
-  credential: true,
-};
-
-app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: true }));
 app.use(
-  '/',
-  express.static(path.join(__dirname, 'public/dist', 'outbody.html')),
+  cors({
+    origin: 'http://52.78.126.125:3000',
+    credentials: true,
+  }),
 );
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static(path.join(__dirname, 'public/dist')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
